369b96ae8243f6bfd253e63658a1ea272480f529
pulgazion <reimarrodri@gmail.com>
2023-01-26 18:56:38 +0000

    Proyecto de Carta Aleatoria

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let randomSymbol = generateRandomSymbol();
+  document.querySelectorAll(".symbolCard").forEach(symbol => {
+    symbol.innerHTML = randomSymbol;
+  });
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".numberCard").innerHTML = generateRandomNumber();
+};
+
+let generateRandomSymbol = () => {
+  let symbolCard = ["♦️", "♥️", "♠️", "♣️"];
+  let indexCard = Math.floor(Math.random() * symbolCard.length);
+  let symbol = symbolCard[indexCard];
+  if (symbol === "♥️" || symbol === "♦️") {
+    symbol = "<span style='color:red'>" + symbol + "</span>";
+  }
+  return symbol;
+};
+
+let generateRandomNumber = () => {
+  let numberCard = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexNumbers = Math.floor(Math.random() * numberCard.length);
+  return numberCard[indexNumbers];
 };
